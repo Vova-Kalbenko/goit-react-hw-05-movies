8a7ada9d6e4f6f5e07a1b735c876e5edf0acba2d
Vova-Kalbenko <lastforhs3@gmail.com>
2023-08-18 12:04:45 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import React, { BrowserRouter } from "react";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <BrowserRouter basename="/https://github.com/Vova-Kalbenko/goit-react-hw-05-movies">
+        <div
+          style={{
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: 40,
+            color: '#010101'
+          }}
+        >
+          React homework template
+        </div>
+      </BrowserRouter>
+    </>
   );
 };
+
+
